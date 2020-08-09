bb38fc0de52e751f8a2dd103ad9161b8ace838a4
Mieszko Manijak <mieszko@grit42.com>
2020-08-09 11:27:49 +0200

    Create react up configured

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          You wake up on a makeshift bed at the wall of your bunker with an unesy feeling in your gut.
+          You know... They are coming.
+        </p>
+          Brace yourself. 
+      </header>
+    </div>
+  );
+}
+
+export default App;

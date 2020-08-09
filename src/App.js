97cdebe95a10cc1887355cb4fc6fc0d8632ecf15
Mieszko Manijak <mieszko@grit42.com>
2020-08-09 12:49:19 +0200

    Flow fcl smoke test successful

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import './App.css';
+import * as sdk from "@onflow/sdk"
+import * as fcl from "@onflow/fcl"
+import * as types from "@onflow/types"
+
+//import './App.css';
+
+
+fcl.config()
+  .put("challenge.handshake", "http://localhost:8701/flow/authenticate")
+  .put("accessNode", "http://localhost:8080")
 
 function App() {
   return (
@@ -9,8 +18,14 @@ function App() {
           You wake up on a makeshift bed at the wall of your bunker with an unesy feeling in your gut.
           You know... They are coming.
         </p>
+        <p>
           Brace yourself. 
+        </p>
       </header>
+      <div className="interactions">
+        <button onClick={fcl.authenticate}>Log in</button>    
+        <button onClick={()=>{console.log('logging out'); fcl.unauthenticate()}}>Log out</button>          
+      </div>
     </div>
   );
 }
---
+++ src/flow.js
@@ -0,0 +1,5 @@
+import * as sdk from "@onflow/sdk"
+import * as fcl from "@onflow/fcl"
+import * as types from "@onflow/types"
+
+

27c83a5adb24ad74771d1a51c4a54c77dfe08356
Mieszko Manijak <mieszko@grit42.com>
2020-08-14 21:46:19 +0200

    get account

--- src/components/buttons/flowscan/index.js
+++ src/components/buttons/flowscan/index.js
@@ -1,1 +1,2 @@
 export {default as LatestBlock} from "./LatestBlock.jsx"
+export {default as GetAccount} from "./GetAccount.jsx"
--- src/components/sections/Flowscan.jsx
+++ src/components/sections/Flowscan.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 
-import {LatestBlock} from "../buttons/flowscan"
+import {LatestBlock, GetAccount} from "../buttons/flowscan"
 
 export default () => <div className="flowscan">
     <p>Flowscan</p>
     <div className="portto">
         <LatestBlock />
+        <GetAccount />
     </div>
 </div>

c55ddda3de43700e4c9ed54a64f366d8ffcf0922
Mieszko Manijak <mieszko@grit42.com>
2020-08-16 10:50:13 +0200

    cross acc minting

--- src/components/buttons/transaction/Mint.js
+++ src/components/buttons/transaction/Mint.js
@@ -1,12 +1,12 @@
-import React from "react"
+import React, {useState} from "react"
 import * as fcl from "@onflow/fcl"
 import * as types from "@onflow/types"
 
 import mintUrl from "../../../cadence/transactions/mint.cdc";
 import execute_transaction_factory from "../../../flow/execute_transaction_factory"
 
-
-const mint = async (classId, targetAddress) => {
+//TODO: extract mint for reuse into transaction handlers
+const mint = async ({classId = 2, targetAddress}) => {
     const NFTStandardAddress = "0x01cf0e2f2f715450"
         , NFTAddress = "0x179b6b1cb6755e31"
     const do_mint = await execute_transaction_factory(mintUrl, {
@@ -16,9 +16,24 @@ const mint = async (classId, targetAddress) => {
     })
     const tx = do_mint([
         fcl.arg(2, types.UInt32),
-        fcl.arg(NFTAddress, types.Address), //TODO: pass targetAddress here
+        fcl.arg(targetAddress, types.Address), //TODO: pass targetAddress here
       ])
-
 }   
 
-export default () => <button onClick={mint}>Mint</button>
+
+
+export default () => {
+    const [selectedAccount, setSelectedAccount] = useState('0x179b6b1cb6755e31')
+
+    const mintSelected = async () => {
+        mint({targetAddress: selectedAccount});
+    }
+
+    return (<div className="mintForm">
+        <button onClick={mintSelected}>Mint</button>
+        <select onChange={e=>{setSelectedAccount(e.target.value)}} value={selectedAccount}>
+            <option value="0x179b6b1cb6755e31">Contract Acc</option>
+            <option value="0xf3fcd2c1a78f5eee">Some Guy or Gal</option>
+        </select>
+    </div>)
+}
